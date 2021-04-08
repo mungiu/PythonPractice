e652088261aabfe67c2bd7cd2c765222342bbada
Andrei Mungiu <andrei_mungiu@yahoo.com>
2021-04-08 17:45:19 +0200

    Practice completed

--- Exercise1.py
+++ Exercise1.py
@@ -1,1 +1,69 @@
+#EX 8.1
+def firstPrint():
+	for count in range(10):
+		print("PepePls")
 
+def secondPrint():
+	i = 0
+	while i < 10:
+		print("NotLikeThis")
+		i += 1
+
+def thirdPrint(wish, qty):
+	for count in range(qty):
+		print(wish)
+
+#def main():
+#	firstPrint()
+#	secondPrint()
+#	thirdPrint(input("input wish: "), int(input("input wish quantity: ")))
+#
+
+
+#EX 8.2 - SUM LIST (imperative VS declarative)
+numbers = [1, 2, 3, 4, 5, 6]
+#imperative aprogramming – focuses on how to execute, defines control flow as statements that change a program state.
+def sum_lst(lst):
+	total = 0
+	for number in lst:
+		total += number
+	return total
+#declarative programming (functional) – focuses on what to execute, defines program logic, but not detailed control flow.
+print(sum(numbers))									#using sum
+import functools
+print(functools.reduce(lambda x, y: x + y, numbers)) #using reduce
+
+
+#EX 8.3 - check for even nnumbers (imperative VS declarative)
+def printEvenInput():
+	temp = int(input())
+	if (temp % 2 == 0):
+		print(temp)
+	printEvenInput()
+#def main():
+#	printEvenInput()
+
+#EX 8.4 - given a list and a groupLength, returns a list of lists with length groupLength.
+def groupList(list, groupLength):
+	count = 1;
+	listOfLists = [];
+	subList = [];
+	for item in list:
+		subList.append(item)
+		if ((count % groupLength == 0)):
+			listOfLists.append(subList.copy())
+			subList = [];
+		count += 1
+	return listOfLists
+#def main():
+#	print(groupList(numbers, 2)) #[[1,2] [3,4] [5,6]]
+
+#EX 8.5 - Create a dictionary specialle which stores software engineering specializations
+dictionarySpecialle = { "Bob":"IoT", "Dora":"Media", "Paw":"Data" }
+dictionarySpecialle["Bob"] = "Data"					# updating "Bob" value
+dictionarySpecialle.update({"Farmer":"Climate"})	# inserts new item to dictionary
+def main():
+	print(dictionarySpecialle.get("Dora"))
+	print(dictionarySpecialle.keys())
+	print(dictionarySpecialle.get("Bob"))
+main()
--- Session1.py
+++ Session1.py
@@ -1,19 +1,19 @@
 # CTRL + Enter executes
-3+7 
+3 + 7 
 #
 type(10)
 #
 type(4.)
 #
-x=3.5
+x = 3.5
 type(x)
 #
 
-# factorial formula n! = n * (n-1)!, n > 0
+# factorial formula n!  = n * (n-1)!, n > 0
 # rec - used for telling F# that this is a recursive function
-def factorial_recursive (x):
+def factorial_recursive(x):
     if x < 1: return 1
-    else: return x * factorial_recursive (x - 1)
+    else: return x * factorial_recursive(x - 1)
 
 while True:
     print(factorial_recursive(int(input("input factorial number: "))))
@@ -22,7 +22,7 @@ def main():
     num = int(input("Enter a positive integer: "))
     # check for validity
     fact = 1
-    for factor in range (num, 1, -1):
+    for factor in range(num, 1, -1):
         fact = fact * factor
     print("the factorial of {0:2} is: {1:2}".format(num, fact))
     #print("The factorial is :", fact, '\n')
