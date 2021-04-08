ce22b6669bcbd683bf09f1aa029b946541e4b2b9
Andrei Mungiu <andrei_mungiu@yahoo.com>
2021-04-08 11:34:29 +0200

    Add project files.

---
+++ PclPython2.py
@@ -0,0 +1,5 @@
+#Run as a reusable module
+
+import PclPython
+
+print("Am done for now!")
---
+++ PclPython3.py
@@ -0,0 +1,8 @@
+#Execution mode in python 3
+# No need to use if __name check
+def main():
+    print("Welcome to PCL1 part 2 -- python version 3")
+
+main()
+
+print("Python is a multi-purpose, paradigm scripting language")
